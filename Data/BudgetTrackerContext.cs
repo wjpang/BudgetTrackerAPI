3768b4b4ca587afb5f937c3223d02fa663ca9985
Wen Juin Pang <pangwenjuin@gmail.com>
2023-12-08 11:50:03 +1100

    Setup EF classes

---
+++ Data/BudgetTrackerContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using BudgetTracker.Models;
+
+namespace BudgetTracker.Data
+{
+    public class BudgetTrackerContext : DbContext
+    {
+        public BudgetTrackerContext(DbContextOptions<BudgetTrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Budget> Budget { get; set; } = default!;
+        public DbSet<Category> Category { get; set; } = default!;
+        public DbSet<User> User { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using BudgetTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BudgetTrackerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BudgetTrackerContext") ?? throw new InvalidOperationException("Connection string 'BudgetContext' not found.")));
+
+// To use SQLite in development and SQL Server in production.
+// if (builder.Environment.IsDevelopment())
+// {
+//     builder.Services.AddDbContext<BudgetTrackerContext>(options =>
+//         options.UseSqlite(builder.Configuration.GetConnectionString("BudgetTrackerContext")));
+// }
+// else
+// {
+//     builder.Services.AddDbContext<BudgetTrackerContext>(options =>
+//         options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionBudgetTrackerContext")));
+// }
 
 // Add services to the container.
 
