2b81d14fa5b151ee1f5baf6f80464e8a88d00fe8
Wen Juin Pang <pangwenjuin@gmail.com>
2023-12-11 14:59:10 +1100

    Seed did not work. So removed

--- Program.cs
+++ Program.cs
@@ -32,13 +32,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    SeedData.Initialise(services);
-}
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
