ef92f57c9071fd68bec1bdb5fb34434313bd3a2e
Wen Juin Pang <pangwenjuin@gmail.com>
2023-12-19 15:12:56 +1100

    Added a db Migrate for SQLite .db file

--- Program.cs
+++ Program.cs
@@ -36,6 +36,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BudgetTrackerContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
